81cd94c943df9611257a91ecae765b9bb2f6c6ee
Bumperguard <bababax@hotmail.de>
2017-01-23 13:42:53 +0100

    RuleRefiner refined
    RuleRefinableCategory: Now has default method buildSpan combining extract and buildArrow
    RuleRefiner: Now uses buildSpan instead of extract and buildArrow

--- FMDE/src/org/upb/fmde/de/rulerefinement/RuleRefinableCategory.java
+++ FMDE/src/org/upb/fmde/de/rulerefinement/RuleRefinableCategory.java
@@ -1,6 +1,7 @@
 package org.upb.fmde.de.rulerefinement;
 
 import org.upb.fmde.de.categories.colimits.pushouts.CategoryWithPushouts;
+import org.upb.fmde.de.categories.colimits.pushouts.Span;
 
 public interface RuleRefinableCategory<Ob, Arr> 
 	extends CategoryWithPushouts<Ob,Arr> {
@@ -18,4 +19,16 @@ public interface RuleRefinableCategory<Ob, Arr>
 	 * @return L_Tilde -> L
 	 */
 	public Arr buildArrow(Ob L_Tilde, Ob L);
+	
+	/**
+	 * @param L
+	 * @param L_Prime
+	 * @return
+	 */
+	default Span<Arr> buildSpan(Ob L, Ob L_Prime) {
+		Ob L_Tilde = this.extract(L, L_Prime);
+		Arr L_Tilde2L = this.buildArrow(L_Tilde, L);
+		Arr L_Tilde2L_Prime = this.buildArrow(L_Tilde, L_Prime);
+		return new Span<Arr>(this, L_Tilde2L, L_Tilde2L_Prime);
+	}
 }
